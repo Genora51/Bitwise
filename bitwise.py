f32f10aa6f2e6758cc834fd553dbf802d2476e22
Genora51 <Geno.racklinasher@gmail.com>
2017-05-19 17:22:30 +0100

    Major overhaul

---
+++ bitwise.py
@@ -0,0 +1,45 @@
+from lib.bitparser import Parse
+from lib.lexer import lex
+from lib.basefuncs import Token, tokens
+import os
+from lib.evaluator import runStates as evaluate
+import hashlib
+
+def h11(w):
+    return hashlib.md5(w.encode()).hexdigest()[:9]
+
+def interpret(text):
+	lexed = lex(text, tokens)
+	parsed = Parse(lexed)
+	evaluate(parsed)
+
+def runFile(fileName):
+	dirf = os.path.dirname(os.path.realpath(fileName))
+	cach = r"%s\__bitcache__\%sc"%(dirf,fileName)
+	mtxt = open(fileName).read()
+	if os.path.isfile(cach):
+		hashed, ptext = open(cach).read().split('\n')
+		if h11(mtxt) == hashed:
+			parsed = eval(ptext)
+		else:
+			parsed = parsNo(mtxt,dirf,cach)
+	else:
+		parsed = parsNo(mtxt,dirf,cach)
+	evaluate(parsed)
+
+def parsNo(mtxt,dirs,cach):
+	try:
+		os.mkdir(dirs + r"\__bitcache__")
+	except OSError:
+		pass
+	lexed = lex(mtxt, tokens)
+	parsed = Parse(lexed)
+	with open(cach,'w') as f:
+		pt = repr(parsed)
+		f.write(h11(mtxt) + '\n')
+		f.write(pt)
+	return parsed
+
+if __name__ == '__main__':
+	import sys
+	runFile(sys.argv[1])
